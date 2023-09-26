5ec61f56b517d6ee4b14f88d7f10dccd68b05154
Amaan Saquib <saquibamaan07@bbdnitm.ac.in>
2023-09-26 12:29:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="padding-global show-overflow">
+            <div className="pricing_testimonial-wrap">
+                <div className="pricing_testimonial-quote">
+                    <div className="pricing_testimonial-left">
+                        <img src="https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/641e7ce3927bd5862f03bcca_quote%20icon.svg" loading="lazy" alt="" class="quote-icon"/>
+                        <div className="h4-24px">Copy.ai has enabled me to free up time to focus more on where we want to be in say three months from now, six months from now, instead of just deep in the weeds.
+                        </div>
+                        <div className="pricing_testimonial-quoter">
+                            <img src="https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/64be93be6187686e979e497d_Jen%20Phillips.jpeg" loading="lazy" alt="" className="pricing_testimonial-img"/>
+                            <div>
+                                <p className="text-body text-weight-medium">Jen Quraishi Phillips</p>
+                                <p className="text-body text-color-gray">Brand Strategy at Airtable</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="pricing_testimonial-right">
+                        <div className="pricing_testimonial-color">
+                        <img src="https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/63cb128457b94e0de7be1a20_2560px-Airtable_Logo.svg.png" loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 22vw, (max-width: 1439px) 24vw, 272.99481201171875px" srcset="https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/63cb128457b94e0de7be1a20_2560px-Airtable_Logo.svg-p-500.png 500w, https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/63cb128457b94e0de7be1a20_2560px-Airtable_Logo.svg-p-800.png 800w, https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/63cb128457b94e0de7be1a20_2560px-Airtable_Logo.svg-p-1080.png 1080w, https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/63cb128457b94e0de7be1a20_2560px-Airtable_Logo.svg-p-1600.png 1600w, https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/63cb128457b94e0de7be1a20_2560px-Airtable_Logo.svg-p-2000.png 2000w, https://assets-global.website-files.com/628288c5cd3e8411b90a36a4/63cb128457b94e0de7be1a20_2560px-Airtable_Logo.svg.png 2560w" alt="" className="pricing_testimonial-logo"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
